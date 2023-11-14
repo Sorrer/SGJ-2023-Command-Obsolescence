73832fdcc65f200b8799f6fac94b60baa8f7205e
Alexander Xie <sorrer@gmail.com>
2023-11-14 03:02:21 -0500

    Added proper path finding tests with variations, nuget package manager, C5 library for prioritiy queues and interval heaps. Still will need to add multithreading for path computation via jobs for optimization (pathfinding is expensive otherwise)

--- Assets/Scripts/Game/Enemies/PathFindingScheduler.cs
+++ Assets/Scripts/Game/Enemies/PathFindingScheduler.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using C5;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Game.Enemies
@@ -11,7 +15,7 @@ namespace Game.Enemies
 
         public struct PathInfo
         {
-            public Stack<TempNode> path;
+            public List<TempNode> path;
             public float totalPathWeight;
         }
         
@@ -20,13 +24,17 @@ namespace Game.Enemies
             public int x;
             public int y;
             public float weight;
+            public float distScore;
+            public float fScore;
             public bool visited = false;
+            
             // Integer cardinal directions
             // 1 2 3
             // 4 5 6
             // 7 8 9
             public TempNode nextNode = null;
-
+            public IPriorityQueueHandle<TempNode> handler;
+            
             public override bool Equals(object obj)
             {
                 // If obj is not TempNode node == null
@@ -37,13 +45,19 @@ namespace Game.Enemies
                 // Only check positions, no layering
                 return node.x == this.x && node.y == this.y;
             }
+
+            public override int GetHashCode()
+            {
+                return $"{x},{y}".GetHashCode();
+            }
         }
 
-        public class TempNodeComparar : IComparer<TempNode>
+        public class TempNodeComparer : IComparer<TempNode>
         {
             public int Compare(TempNode x, TempNode y)
             {
-                var cal = x.weight - y.weight;
+                
+                var cal = x.fScore - y.fScore;
 
                 if (cal < 0) return -1;
                 if (cal > 0) return 1;
@@ -51,11 +65,11 @@ namespace Game.Enemies
             }
         }
         
-        public PathInfo GeneratePath(TempNode[,] world, Vector2Int startingPosition, Vector2Int finishPosition)
+        public static PathInfo GeneratePath(ref TempNode[,] world, Vector2Int startingPosition, Vector2Int finishPosition)
         {
 
             // Using queue for breadth-first search, depth search will result in a quicker execution time, but the first answer being correct.
-            SortedSet<TempNode> unvisitedNodes = new SortedSet<TempNode>(new TempNodeComparar());
+            IntervalHeap<TempNode> unvisitedNodes = new IntervalHeap<TempNode>(new TempNodeComparer());
             int xWidth = world.GetLength(0);
             int yHeight = world.GetLength(1);
 
@@ -63,24 +77,38 @@ namespace Game.Enemies
             {
                 for (int y = 0; y < yHeight; y++)
                 {
-                    world[x, y].weight = float.PositiveInfinity;
-                    unvisitedNodes.Add(world[x, y]);
+                    if (!CanTraverse(world[x, y])) continue;
+                    world[x, y].distScore = float.PositiveInfinity;
+                    world[x, y].fScore = float.PositiveInfinity;
+                    world[x, y].nextNode = null;
+
+                    if (x == startingPosition.x && y == startingPosition.y)
+                    {
+                        world[x, y].distScore = 0;
+                        world[x, y].fScore = 0;
+                    }
                 }
             }
+            unvisitedNodes.Add(ref world[startingPosition.x, startingPosition.y].handler, world[startingPosition.x, startingPosition.y]);
+
+            bool found = false;
             
             while (unvisitedNodes.Count > 0)
             {
+                Debug.Log("DOING AN ITERATION");
                 // Traverse the shortest constant path
-                var node = unvisitedNodes.Min;
-                unvisitedNodes.Remove(node);
+                var node = unvisitedNodes.DeleteMin();
+                Debug.Log("Unvisited count: " + unvisitedNodes.Count);
                 node.visited = true;
 
+                
                 if (node.x == finishPosition.x && node.y == finishPosition.y)
                 {
+                    Debug.Log("Found solution");
                     // Solution found
+                    found = true;
                     break;
                 }
-                
 
                 // Node the most cleanest looking code
                 // Mostly looks this way because the actual nodes can be hard coded in position, otherwise it would be a neighbor list..
@@ -89,26 +117,34 @@ namespace Game.Enemies
                 // 1 2 3
                 // 4 5 6
                 // 7 8 9
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x - 1, node.y + 1);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x, node.y + 1);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x + 1, node.y + 1);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x - 1, node.y);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x + 1, node.y);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x - 1, node.y - 1);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x , node.y - 1);
-                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x + 1, node.y - 1);
+                //ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x - 1, node.y + 1, finishPosition, 3);
+                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x, node.y + 1, finishPosition);
+                //ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x + 1, node.y + 1, finishPosition, 3);
+                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x - 1, node.y, finishPosition,3);
+                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x + 1, node.y, finishPosition,3);
+                //ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x - 1, node.y - 1, finishPosition, 3);
+                ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x , node.y - 1, finishPosition);
+                //ProcessPathNode(ref unvisitedNodes, ref world, xWidth, yHeight, node.x, node.y, node.x + 1, node.y - 1, finishPosition, 3);
 
 
             }
-            
+
+            if (!found)
+            {
+                return new PathInfo()
+                {
+                    path = null,
+                    totalPathWeight = -1
+                };
+            }
             
             // Generate best path by getting the finishPosition
-            Stack<TempNode> bestPath = new Stack<TempNode>();
+            List<TempNode> bestPath = new List<TempNode>();
             float totalPathWeight = 0;
             TempNode nextNode = world[finishPosition.x, finishPosition.y];
-            while (nextNode != null && (nextNode.x == startingPosition.x && nextNode.y == startingPosition.y))
+            while (nextNode != null)
             {
-                bestPath.Push(nextNode);
+                bestPath.Insert(0, nextNode);
                 totalPathWeight += nextNode.weight;
                 nextNode = nextNode.nextNode;
             }
@@ -123,40 +159,48 @@ namespace Game.Enemies
 
 
         
-        private void ProcessPathNode(ref SortedSet<TempNode> unvisitedNodes, ref TempNode[,] world, int xWidth, int yHeight, int curX, int curY, int neighborX, int neighborY)
+        private static void ProcessPathNode(ref IntervalHeap<TempNode> unvisitedNodes, ref TempNode[,] world, int xWidth, int yHeight, int curX, int curY, int neighborX, int neighborY, Vector2Int finishPoint, int addedWeight = 0)
         {
             // Out of bounds
-            if (neighborX < 0 || neighborX >= xWidth || neighborY < 0 && neighborY >= yHeight) return;
+            if (neighborX < 0 || neighborX >= xWidth || neighborY < 0 || neighborY >= yHeight) return;
 
             // Already visited we can ignore
             
 
             if (!CanTraverse(world[neighborX, neighborY])) return;
 
-            if (!world[neighborX, neighborY].visited) return;
-            
-            var weight = world[curX, curY].weight + GetWeight(world[neighborX, neighborY]);
+            if (world[neighborX, neighborY].visited) return;
 
-            if (weight < world[neighborX, neighborY].weight)
+            if (world[neighborX, neighborY].handler == null)
+            {
+                unvisitedNodes.Add(ref world[neighborX, neighborY].handler, world[neighborX, neighborY]);
+            }
+            
+            var distScore = world[curX, curY].distScore + GetWeight(world[neighborX, neighborY]) + addedWeight;
+            Debug.Log($"{world[curX, curY].distScore} - {world[neighborX, neighborY].distScore} ({curX},{curY}) ({neighborX},{neighborY}) {distScore}");
+            if (distScore < world[neighborX, neighborY].distScore)
             {
                 // Resort this item, since it has a new weight now (2(log n), but this will happen o(b) b=best path length)
-                world[neighborX, neighborY].weight = weight;
+                world[neighborX, neighborY].distScore = distScore;
                 world[neighborX, neighborY].nextNode = world[curX, curY];
+                world[neighborX, neighborY].fScore =
+                    Mathf.Pow(finishPoint.x - neighborX, 2) + Mathf.Pow(finishPoint.y - neighborY, 2)  + distScore;
                 
-                unvisitedNodes.Remove(world[neighborX, neighborY]);
-                unvisitedNodes.Add(world[neighborX, neighborY]);
+                if(world[neighborX,neighborY].handler != null) unvisitedNodes.Delete(world[neighborX, neighborY].handler);
+                unvisitedNodes.Add(ref world[neighborX, neighborY].handler, world[neighborX, neighborY]);
+
             }
             
         }
 
         
 
-        public bool CanTraverse(TempNode node)
+        public static bool CanTraverse(TempNode node)
         {
-            return node.weight == -1;
+            return node.weight >= 0;
         }
 
-        public float GetWeight(TempNode node)
+        public static float GetWeight(TempNode node)
         {
             // TODO: Describe weight best on how hard it is to kill the object + total spaces on the map (so it chooses to move first)
             // TODO: If we want to include hotspots as goals to destroy, we must determine that either the node itself has the lowest value in the system, so it will bias its movement towards it. Or the path planner should get the paths to the hotspots, and check if they are within bias range to focus and attack.
---
+++ Assets/Scripts/Game/Enemies/TestPathFindingBehaviour.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace Game.Enemies
+{
+    public class TestPathFindingBehaviour : MonoBehaviour
+    {
+
+        public int width = 15;
+        public int height = 15;
+
+        private PathFindingScheduler.TempNode[,] cleanMaze = null;
+        private PathFindingScheduler.TempNode[,] maze = null;
+        private Vector2Int startPosition;
+        private Vector2Int endPosition;
+        private List<PathFindingScheduler.TempNode> path;
+        private List<PathFindingScheduler.TempNode> bestPath;
+
+        private void Start()
+        {
+            GenerateMaze();
+        }
+
+        private void Update()
+        {
+            GenerateMaze();
+        }
+
+        [ContextMenu("Generate Maze")]
+        public void GenerateMaze()
+        {
+
+            List<Vector2Int> validPosition = new List<Vector2Int>();
+            PathFindingScheduler.TempNode[,] nodes = new PathFindingScheduler.TempNode[width, height];
+            PathFindingScheduler.TempNode[,] cleanNodes = new PathFindingScheduler.TempNode[width, height];
+            for(int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var val =  Random.value > 0.8 ? -1 : 1 + Random.Range(2,1000);
+                    var node = new PathFindingScheduler.TempNode();
+                    node.x = x;
+                    node.y = y;
+                    node.weight = val;
+                    nodes[x, y] = node;
+
+                    if (node.weight >= 1)
+                    {
+                        validPosition.Add(new Vector2Int(x,y));
+                    }
+                    
+                    node = new PathFindingScheduler.TempNode();
+                    node.x = x;
+                    node.y = y;
+                    node.weight = val >= 1 ? 1 : -1;
+                    cleanNodes[x, y] = node;
+                }
+            }
+
+            startPosition = validPosition[Random.Range(0, validPosition.Count)];
+            endPosition = validPosition[Random.Range(0, validPosition.Count)];
+
+            maze = nodes;
+            cleanMaze = cleanNodes;
+
+            var data = PathFindingScheduler.GeneratePath(ref maze, startPosition, endPosition);
+
+            path = data.path;
+            
+            data = PathFindingScheduler.GeneratePath(ref cleanMaze, startPosition, endPosition);
+
+            bestPath = data.path;
+
+        }
+
+
+        private void OnDrawGizmos()
+        {
+
+            float maxDistScore = 0;
+            float maxWeightScore = 0;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (float.IsPositiveInfinity(maze[x, y].distScore)) continue;
+                    if (maze[x, y].distScore > maxDistScore) maxDistScore = maze[x, y].distScore;
+                    if (maze[x, y].weight > maxWeightScore) maxWeightScore = maze[x, y].weight;
+
+                    Gizmos.color = maze[x, y].visited ? Color.white : Color.black;
+                    Gizmos.DrawWireCube(new Vector3(x, y, -10), new Vector3(1,1,1));
+                }
+            }
+
+            // Draw maze
+            for(int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    var node = maze[x, y];
+                        
+                    if (node != null)
+                    {
+
+
+                        if (node.weight < 0)
+                        {
+                            Gizmos.color = Color.black;
+                            Gizmos.DrawCube(new Vector3(node.x, node.y, 0), new Vector3(1,1,1));
+                        }
+                        else
+                        {
+                            if (float.IsPositiveInfinity(node.distScore))
+                            {
+                                Gizmos.color = Color.yellow;
+                                if (node.visited)
+                                {
+                                    Gizmos.DrawWireSphere(new Vector3(node.x, node.y, 0), 0.5f);
+                                }
+                                else
+                                {
+                                    Gizmos.DrawWireCube(new Vector3(node.x, node.y, 0), new Vector3(1,1,1));
+                                }
+                            }
+                            else
+                            {
+                                Gizmos.color = Color.Lerp(Color.white, Color.red, node.distScore / maxDistScore);
+                                Gizmos.DrawSphere(new Vector3(node.x, node.y, 0), 0.5f);
+                                Gizmos.color = Color.Lerp(Color.white, Color.blue, node.weight / maxWeightScore);
+                                Gizmos.DrawSphere(new Vector3(node.x, node.y, 2.0f), 0.2f);
+                                
+                            }
+                        }
+                        
+                    }
+                }
+            }
+            
+            
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(new Vector3(startPosition.x, startPosition.y, 0), new Vector3(1,1,1));
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(new Vector3(endPosition.x, endPosition.y, 0), new Vector3(1,1,1));
+
+            if (path != null)
+            {
+                foreach (var node in path)
+                {
+                    Gizmos.color = Color.magenta;
+                    Gizmos.DrawCube(new Vector3(node.x, node.y, 1.0f), Vector3.one * 0.1f);
+                }
+            }
+            
+            if (bestPath != null)
+            {
+                foreach (var node in bestPath)
+                {
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawCube(new Vector3(node.x, node.y, 2.0f), Vector3.one * 0.2f);
+                }
+            }
+        }
+        
+    }
+}
