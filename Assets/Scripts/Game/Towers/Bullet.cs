81b89bbbb37ef946919eb8d4bdc36d56111dfb2b
Matt Kataryniak <mwk9@njit.edu>
2023-11-15 23:57:06 -0500

    Finished first tower type. Directions for towers. Bug fixes.

---
+++ Assets/Scripts/Game/Towers/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : Projectile
+{
+	// Start is called before the first frame update
+	protected override void Start()
+	{
+		base.Start();
+	}
+
+	protected override void FixedUpdate()
+	{
+		base.FixedUpdate();
+		//if (rb != null)
+		//	rb.velocity = transform.up * speed;
+		transform.Translate(Vector3.up * speed * Time.deltaTime);
+	}
+}
