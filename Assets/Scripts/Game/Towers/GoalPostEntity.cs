0986ac41979f3c4666aa0c0df15f2b9835a9904c
Justuna <jlpisa999@yahoo.com>
2023-11-17 19:41:42 -0500

    fixed more typos

--- Assets/Scripts/Game/Towers/GoalPostEntity.cs
+++ Assets/Scripts/Game/Towers/GoalPostEntity.cs
@@ -1,15 +1,15 @@
-namespace DefaultNamespace;
+using Game.Enemies;
 
 public class GoalPostEntity : TileEntity
 {
     public override void OnTowerDestroy()
     {
-        SpawnEnemyBehaviour.Instance.mapReference.RemoveGoalPost(LevelGenerator.Instance.GetGridPosition(this.transform.position))
+        SpawnEnemyBehaviour.Instance.mapReference.RemoveGoalPost(LevelGenerator.Instance.GetGridPosition(this.transform.position));
     }
     
     public override void OnTowerPlaced()
     {
-        SpawnEnemyBehaviour.Instance.mapReference.AddGoalPost(LevelGenerator.Instance.GetGridPosition(this.transform.position))
+        SpawnEnemyBehaviour.Instance.mapReference.AddGoalPost(LevelGenerator.Instance.GetGridPosition(this.transform.position));
     }
     
     
